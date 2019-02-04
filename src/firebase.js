cc9c8ad2cdc3ade4ba0c5535b168c16be1de688a
Mitchell <mitchellsutton2@gmail.com>
2019-02-05 09:56:09 +1100

    commit message

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase";
+
+const config = {
+  apiKey: "AIzaSyDIHT0TpN_nCi0fPg4H6VJ81PXrkysu6Mo",
+  authDomain: "example-767bc.firebaseapp.com",
+  databaseURL: "https://example-767bc.firebaseio.com",
+  projectId: "example-767bc",
+  storageBucket: "example-767bc.appspot.com",
+  messagingSenderId: "160796803539"
+};
+firebase.initializeApp(config);
+export default firebase;
