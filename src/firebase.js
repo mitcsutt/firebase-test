3546f07e774ec8d827ab93d579bde9d635379faa
Mitchell Sutton <42919522+mitcsutt@users.noreply.github.com>
2019-02-05 09:58:04 +1100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from "firebase";
 
 const config = {
-  apiKey: "AIzaSyDIHT0TpN_nCi0fPg4H6VJ81PXrkysu6Mo",
-  authDomain: "example-767bc.firebaseapp.com",
-  databaseURL: "https://example-767bc.firebaseio.com",
-  projectId: "example-767bc",
-  storageBucket: "example-767bc.appspot.com",
-  messagingSenderId: "160796803539"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 };
 firebase.initializeApp(config);
 export default firebase;
